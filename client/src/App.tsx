9b57770c8ce51c7f27d978842a1410a2cfa8bc40
Santeri <santeri.kaaranto@gmail.com>
2024-01-28 22:14:03 +0200

    Geocoded database data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import Reac, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 // Jotta marker iconit toimii reactin kanssa
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-import 'leaflet-defaulticon-compatibility';
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
+import "leaflet-defaulticon-compatibility";
 
 interface GasStation {
   id: number;
@@ -13,34 +13,39 @@ interface GasStation {
   fuel_95E10: number;
   fuel_98E: number;
   fuel_Di: number;
+  lat: number;
+  lon: number;
 }
 
 const App: React.FC = () => {
-  const position: [number, number] = [62.78882825247504, 22.82214852702566]; 
+  const position: [number, number] = [62.78882825247504, 22.82214852702566];
 
   const [gasStations, setGasStations] = useState<GasStation[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/gas_stations')
+    fetch("http://localhost:3001/api/gas_stations")
       .then((response) => response.json())
       .then((data) => {
-        setGasStations(data); // Uncomment this line
+        setGasStations(data);
         console.log(data);
       })
-      .catch((error) => console.error('Error fetching gas stations data:', error));
+      .catch((error) =>
+        console.error("Error fetching gas stations data:", error)
+      );
   }, []);
-  
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker position={position}>
-        <Popup>
-          SeAMK.
-        </Popup>
+        <Popup>SeAMK.</Popup>
       </Marker>
     </MapContainer>
   );
--- server/locations.sql
+++ server/locations.sql
@@ -5,22 +5,24 @@ CREATE TABLE IF NOT EXISTS gas_stations (
     corp TEXT NOT NULL,
     fuel_95E10 REAL NOT NULL,
     fuel_98E REAL NOT NULL,
-    fuel_Di REAL NOT NULL
+    fuel_Di REAL NOT NULL,
+    lat REAL NOT NULL,
+    lon REAL NOT NULL
 );
 
 -- Insert data into the gas_stations table
-INSERT INTO gas_stations (addr, corp, fuel_95E10, fuel_98E, fuel_Di) VALUES
-    ('Ruukintie 177, 60200 Seinajoki, Finland', 'ABC', 2.106, 2.200, 2.148),
-    ('Hyllykalliontie 1, 60510 Seinajoki, Finland', 'St1', 2.149, 2.219, 2.379),
-    ('Vapaudentie 73, 60100 Seinajoki, Finland', 'Neste', 2.119, 2.219, 2.249),
-    ('Valtionkatu 7, 60100 Seinajoki, Finland', 'Shell', 2.121, 2.231, 2.156),
-    ('Verkatehtaankatu 13, 60100 Seinajoki, Finland', 'ABC', 2.108, 2.207, 2.183),
-    ('Vapaudentie 25, 60100 Seinajoki, Finland', 'Neste', 2.134, 2.234, 2.269),
-    ('Kasperinviita 15, 60150 Seinajoki, Finland', 'Neste', 2.114, 2.204, 2.244),
-    ('Tapiolantie 10, 60150 Seinajoki, Finland', 'St1', 2.114, 2.204, 2.244),
-    ('Kytopolku 2, 60320 Seinajoki, Finland', 'ABC', 2.149, 2.249, 2.289),
-    ('Bullerintie 2, 60200 Seinajoki, Finland', 'ABC', 2.149, 2.249, 2.289),
-    ('Kivistontie 30, 60100 Seinajoki, Finland', 'ABC', 2.066, 2.151, 2.095),
-    ('Hyllykalliontie 2, 60510 Seinajoki, Finland', 'ABC', 2.075, 2.168, 2.104),
-    ('Paivolankatu 39, 60120 Seinajoki, Finland', 'St1', 2.056, 2.146, 2.083);
+INSERT INTO gas_stations (addr, corp, fuel_95E10, fuel_98E, fuel_Di, lat, lon) VALUES
+    ('Ruukintie 177, 60200 Seinajoki, Finland', 'ABC', 2.106, 2.200, 2.148, 62.75269170000001, 22.8661126),
+    ('Hyllykalliontie 1, 60510 Seinajoki, Finland', 'St1', 2.149, 2.219, 2.379, 62.80566400000001, 22.880002),
+    ('Vapaudentie 73, 60100 Seinajoki, Finland', 'Neste', 2.119, 2.219, 2.249, 62.7923552, 22.8326001),
+    ('Valtionkatu 7, 60100 Seinajoki, Finland', 'Shell', 2.121, 2.231, 2.156, 62.7939149, 22.8388329),
+    ('Verkatehtaankatu 13, 60100 Seinajoki, Finland', 'ABC', 2.108, 2.207, 2.183, 62.7948549, 22.8315675),
+    ('Vapaudentie 25, 60100 Seinajoki, Finland', 'Neste', 2.134, 2.234, 2.269, 62.7772855, 22.8414135),
+    ('Kasperinviita 15, 60150 Seinajoki, Finland', 'Neste', 2.114, 2.204, 2.244, 62.7712653, 22.877934),
+    ('Tapiolantie 10, 60150 Seinajoki, Finland', 'St1', 2.114, 2.204, 2.244, 62.7731803, 22.8772993),
+    ('Kytopolku 2, 60320 Seinajoki, Finland', 'ABC', 2.149, 2.249, 2.289, 62.78630510000001, 22.8060556),
+    ('Bullerintie 2, 60200 Seinajoki, Finland', 'ABC', 2.149, 2.249, 2.289, 62.7496512, 22.8257286),
+    ('Kivistontie 30, 60100 Seinajoki, Finland', 'ABC', 2.066, 2.151, 2.095, 62.77630879999999, 22.8509447),
+    ('Hyllykalliontie 2, 60510 Seinajoki, Finland', 'ABC', 2.075, 2.168, 2.104, 62.804804, 22.880417),
+    ('Paivolankatu 39, 60120 Seinajoki, Finland', 'St1', 2.056, 2.146, 2.083, 62.7923978, 22.8696157);
 
