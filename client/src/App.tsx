851747e8ae2e56a5c0b5f296c12c5d6bbe82e9ac
Santeri <santeri.kaaranto@gmail.com>
2024-01-28 21:18:07 +0200

    Luetaan data palvelimelta

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,30 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
 
+interface GasStation {
+  id: number;
+  addr: string;
+  corp: string;
+  fuel_95E10: number;
+  fuel_98E: number;
+  fuel_Di: number;
+}
+
 const App: React.FC = () => {
   const position: [number, number] = [62.78882825247504, 22.82214852702566]; 
+
+  const [gasStations, setGasStations] = useState<GasStation[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/gas_stations')
+      .then((response) => response.json())
+      .then((data) => {
+        setGasStations(data); // Uncomment this line
+        console.log(data);
+      })
+      .catch((error) => console.error('Error fetching gas stations data:', error));
+  }, []);
+  
 
   return (
     <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100%' }}>
